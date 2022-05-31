0f039ef0e7754e0acbd68fe7b604260fb91da883
Samuel-17 <93955414+Samuel-17@users.noreply.github.com>
2022-05-31 01:38:40 -0300

    Update conexao.php

--- form/conexao.php
+++ form/conexao.php
@@ -8,19 +8,46 @@ try{
 }catch (Exception $e){
     echo "Erro generico ".$e->getMessage();
 }
+// ==============================================================================>
 //Insert data
-
 //mode 1
-$user = "joãobatista177@gmail.com";
-$password = '8888888888';
+// $user = "samuelvittor27@gmail.com";
+// $password = '12345';
 
-$res = $pdo->prepare("INSERT INTO pessoa(nome,senha) VALUES (:n,:p)");
+// $res = $pdo->prepare("INSERT INTO pessoa(nome,senha) VALUES (:n,:p)");
 
-$res->bindValue(":n",$user);
-$res->bindValue(":p",$password);
-$res->execute();
+// $res->bindValue(":n",$user);
+// $res->bindValue(":p",$password);
+// $res->execute();
 
 // //mode 2
 // $pdo->query("INSERT INTO pessoa(nome, senha) VALUES ('Vittor','12345')");
+// ==============================================================================>
+
+// ====================================DELETE==========================================>
+
+//mode 1
+// $databasecomand=$pdo->prepare("DELETE FROM pessoa WHERE id = :id");
+// $id = 2;
+// $databasecomand->bindValue(":id",$id);
+// $databasecomand->execute();
+
+//mode 2
+// $databasecomand=$pdo->query("DELETE FROM pessoa WHERE id='3'");
+// ====================================UPDATE==========================================>
+//mode 1
+// $databasecomand = $pdo->prepare("UPDATE pessoa SET nome = :nam WHERE id = :id");
+// $databasecomand->bindValue(":nam","Vittor22@gmail.com");
+// $databasecomand->bindValue(":id","5");
+// $databasecomand->execute();
+
+//mode 2
+// $databasecomand = $pdo->query("UPDATE pessoa SET nome = 'vittor' WHERE id = '4' ");
+
+// ====================================SELECT==========================================>
+
+
+
+
 
 ?>
